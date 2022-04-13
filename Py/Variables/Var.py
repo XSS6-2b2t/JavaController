b6f7ff03d61617a2c22843b0ccc670e39ca120a7
GetForkedNerd <72362725+GetForkedNerd@users.noreply.github.com>
2022-04-13 17:01:24 -0600

    Create  Var.py

---
+++ Py/Variables/Var.py
@@ -0,0 +1,14 @@
+from time import sleep
+import os
+import sys
+import random
+print("Generating Test coords List")
+print("These arent real coordinates")
+print("this exploit is closed source, as if it was open source it would get patched by tomorrow")
+
+
+
+
+CoordsList = ran.randint(-10000000, 1000000)
+CoordsList2 = ran.randint(3, 180)
+CoordsList3 = ran.randint(-10000000, 10000000)
